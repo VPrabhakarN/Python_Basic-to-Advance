d8fa625d685b7543b9615cf730c80bafa6c0815f
VPrabhakarN <vijaynagane19@gmail.com>
2025-12-14 02:07:45 +0530

    contains the definition of variables in Python

---
+++ Class 1/Variables.py
@@ -0,0 +1,9 @@
+# Variables are used to store something in python, Variable can be initialized when we assign a value to it.
+age = 21 
+print(age)
+
+name = "Viay Prabhakar Nagane"
+print(name) 
+
+marks = 90.60
+print(marks) 
