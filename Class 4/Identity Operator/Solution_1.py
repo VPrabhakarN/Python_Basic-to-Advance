f5539eae274dbdd1403e69946f162e0d51eae46b
VPrabhakarN <vijaynagane19@gmail.com>
2026-01-31 18:54:24 +0530

    Got solution

---
+++ Class 4/Identity Operator/Solution_1.py
@@ -0,0 +1,6 @@
+a = 2
+b = 2
+
+print(a is b)
+
+# Ans : True -> Small integers are cached in Python, so 'a' and 'b' refer to the same object in memory.
---
+++ Class 4/Identity Operator/Solution_2.py
@@ -0,0 +1,6 @@
+a = 2
+b = 3
+
+print(a is not b)
+
+# Ans : True -> 'a' and 'b' refer to different objects in memory, so 'a is not b' evaluates to True.
