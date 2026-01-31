75d4104fd0684d26298671c54b2badef6d4d9096
VPrabhakarN <vijaynagane19@gmail.com>
2026-01-31 21:42:08 +0530

    Got solution of question 3

---
+++ 3. Control Statements/elif_statement/Solution_3.py
@@ -0,0 +1,15 @@
+# Qyestion : Even or Odd Checker -> Accept the number from the user and check whether it's even or odd using modulus.
+
+# Taking input from the user 
+num = int(input("Enter a number : "))
+
+# Using elif statement to check the conditions
+if num % 2 == 0:
+    print(f"{num} is an Even number")
+elif num % 2 != 0:
+    print(f"{num} is an Odd number")
+else:
+    print("Invalid Input !!! Please enter a valid number !!")
+
+    
+
