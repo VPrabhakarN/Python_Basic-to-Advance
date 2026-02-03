acf84907c033108009b740c76f49f81e7bba992a
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-03 13:55:39 +0530

    Solution 6

---
+++ 3. Control Statements/1. If_statement/Solution_6.py
@@ -0,0 +1,8 @@
+# Write a program to check whether a number is in the range 1 to 100.
+
+# Taking input from the user 
+number = int(input("Enter the number : "))
+
+# Using if statement to check the condition 
+if number >= 1 and number <= 100 :
+    print(f"{number} is in the range 1 to 100.")
