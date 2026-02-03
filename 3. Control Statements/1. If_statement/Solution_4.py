e59cbaa7b63a2ba384d377cf106ac01681441c27
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-03 13:40:50 +0530

    Solution 4

---
+++ 3. Control Statements/1. If_statement/Solution_4.py
@@ -0,0 +1,6 @@
+# Question :  Program to print positive number entered by the user 
+num = int(input("Enter the number : "))
+
+# Using if statement to check the condition 
+if num > 0 :
+    print(f"{num} is positive number !")
