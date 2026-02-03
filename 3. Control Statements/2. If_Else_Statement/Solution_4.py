5e069b401b7548b0fb68d2b63b0bbd016212bb50
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-03 17:29:30 +0530

    Got solution of question 4

--- 3. Control Statements/2. If_Else_Statement/Solution_4.py
+++ 3. Control Statements/2. If_Else_Statement/Solution_4.py
@@ -1,12 +1,10 @@
 # Question : Write a program to check the number is positive or negative 
 
 # Taking the input from the user 
-ch = input("Enter the value : ")
+num = int(input("Enter the number : ")) 
 
-# Using If-else statement to check the condition 
-if (ch >= 'a' or ch <= 'z') or (ch >= 'A' or ch <= 'Z') :
-    print(f"{ch} is a alphabet !!")
-elif '0' <= ch >= '9' :
-    print(f"{ch} is a digit !")
+# Usinf if-else statement to check the number is positive or negative
+if num > 0 :
+    print(f"{num} is positive number!!")
 else :
-    print(f"{ch} is a special character !")
+    print(f"{num} is negative number or zero!!")
