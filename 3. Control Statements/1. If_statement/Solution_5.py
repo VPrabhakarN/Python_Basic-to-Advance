f392e3ecd2e3a95583705fe7032a742aa952b747
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-03 13:45:23 +0530

    Solution 5

---
+++ 3. Control Statements/1. If_statement/Solution_5.py
@@ -0,0 +1,8 @@
+# Question : Write a progran to identify that user can vote or not 
+
+# Taking the input from the user 
+age = int(input("Enter the age : "))
+
+# Using if statement to check the condition 
+if age >= 18  :
+    print("You can vote !")
