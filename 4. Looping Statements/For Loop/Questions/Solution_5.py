43f0e4897444270ec5fe11158e9541cdc9084cdf
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-04 23:44:41 +0530

    Got solution for question 5

---
+++ 4. Looping Statements/For Loop/Questions/Solution_5.py
@@ -0,0 +1,7 @@
+# Question : Write a program to print first 10 even numbers.
+
+print("Even Numbers : ", end="")
+
+# Usinng for loop to print first 10 even numbers
+for i in range(2, 21, 2) :
+    print(i, end=", ")
