6923f344418b08266d2140bbe88585a2333833a3
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-04 23:40:44 +0530

    Got solution for question 4

---
+++ 4. Looping Statements/For Loop/Questions/Solution_4.py
@@ -0,0 +1,7 @@
+# Question : Write a program to print first 10 natural numbers 
+
+print("Natural Number : ", end="")
+
+# Using for loop to print first 10 natural numbers
+for i in range (1, 11, 1) :
+    print(i, end=", ")
