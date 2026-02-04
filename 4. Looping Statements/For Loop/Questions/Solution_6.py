3522b2a92cf5b8a3a415ca5b38a34428639bfaf0
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-05 00:25:32 +0530

    Got solution for question 6

---
+++ 4. Looping Statements/For Loop/Questions/Solution_6.py
@@ -0,0 +1,8 @@
+# Question : Write a program to print first 10 odd numbers.
+
+print("Odd Numbers : ", end="")
+
+# Using for loop to print first 10 odd numbers 
+for i in range (1, 20, 2) :
+    print(i, end=" ")
+
