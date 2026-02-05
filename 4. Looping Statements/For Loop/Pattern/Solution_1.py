65f4eef0c3e51a76ea49cdaee4c7decc6bdf247a
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-05 18:38:23 +0530

    Got solution for Question-1 - Pattern Module

---
+++ 4. Looping Statements/For Loop/Pattern/Solution_1.py
@@ -0,0 +1,8 @@
+# Question : Print a pattern of stars using nested for loops.
+
+# Using for loop to print a pattern of starts using nested for loops
+for i in range (0, 5, 1) :
+    for j in range(0, i+1, 1):
+        print("*", end=" ")
+    print()
+  
