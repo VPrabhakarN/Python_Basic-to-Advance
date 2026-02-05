640d91372bd5eb3e8ed05ffa90b854f80848e61f
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-05 12:30:59 +0530

    Got solution for question 1 - string module

---
+++ 4. Looping Statements/For Loop/String/Solution_1.py
@@ -0,0 +1,9 @@
+# Question : Print characters of a string using a for loop
+
+# Taking the string from the user 
+text = input("Enter a string : ")
+
+# Printing the character of a string using a foor loop 
+for i in text :
+    print(f"{i}", end=" ")
+
