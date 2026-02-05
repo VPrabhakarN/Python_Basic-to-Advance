3af972be82e40aa75b9347e7e473460b6a80247e
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-05 20:18:18 +0530

    Got solution for question 2 - Pattern Module

---
+++ 4. Looping Statements/For Loop/Pattern/Solution_2.py
@@ -0,0 +1,16 @@
+# Square Star Pattern
+"""
+*****
+*****
+*****
+
+"""
+
+# Taking value from the user 
+rows = int(input("Enter the number of rows : "))
+colomn = int(input("Enter the number of coloms : "))
+
+for i in range(0, rows, 1) :
+    for j in range(0, colomn, 1) :
+        print("*", end=" ")
+    print()
