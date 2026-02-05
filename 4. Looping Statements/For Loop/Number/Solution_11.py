6f830612a649fa9e105a883975213dac06bb8db5
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-05 12:25:51 +0530

    Got solution for question 11

---
+++ 4. Looping Statements/For Loop/Number/Solution_11.py
@@ -0,0 +1,9 @@
+# Question : Calculate the sum of numbers from 1 to 10 using a for loop.
+sum = 0
+
+# Using for loop to calculate the sum of numbers from 1 to 100 
+for i in range(1, 101, 1) :
+    sum = sum + i
+
+# Printing the sum of numbers from 1 to 100 
+print(f"Sum of numbers from 1 to 100 : {sum}")
