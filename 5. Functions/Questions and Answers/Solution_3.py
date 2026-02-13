5d210afc55b1e446b2b4503906c80f286022c737
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-13 21:04:39 +0530

    Got Solution

---
+++ 5. Functions/Questions and Answers/Solution_3.py
@@ -0,0 +1,9 @@
+# Function 
+def user_name(name) :
+    print(f"Hello {name}! Have a great day ahead!")
+
+# Taking the input from the user 
+name = input("Enter your name : ")
+
+# Calling the function
+user_name(name)
