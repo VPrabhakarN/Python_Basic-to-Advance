414e995a81c58de9efc78afc6809e0ca8b4963bb
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-14 13:14:22 +0530

    Solved Question-5

---
+++ 5. Functions/Questions and Answers/Solution_5.py
@@ -0,0 +1,13 @@
+# Function
+def sum_of (a, b) :
+    return a+b
+
+# Taking the input from the user
+a = int(input("Enter 1st value : "))
+b = int(input("Enter 2nd value : "))
+
+# Calling function to add both values
+result = sum_of(a, b)
+
+# Displaying the result
+print(f"Addition of {a} and {b} is {result}.")
