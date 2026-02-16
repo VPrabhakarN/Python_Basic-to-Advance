db782ba398b44304dfe017753bbb3aee026e5100
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-16 20:06:59 +0530

    remove()

---
+++ 6. Data Structures/Lists/Methods/6_remove.py
@@ -0,0 +1,12 @@
+# Defining the list 
+a = [10, 20, 30, 30, 40, 50]
+
+# Removing the first element using 'remove()' method
+a.remove(30)
+
+# Displaying the list
+print(a)
+
+# Iterating the list 
+for item in a :
+    print(item, end=" ")
