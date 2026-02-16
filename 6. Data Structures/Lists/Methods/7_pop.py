b9c5ac96a5b6c7e97ff5c69a69b6315f0208f66a
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-16 20:12:30 +0530

    pop()

---
+++ 6. Data Structures/Lists/Methods/7_pop.py
@@ -0,0 +1,11 @@
+# Defining the list
+a = [10, 20, 30, 40, 50]
+
+# pooping an element from the list using 'pop()' method
+popped_item = a.pop(2)
+
+# Displaying the popped element  
+print(f"Popped Element : {popped_item}")
+
+# Displaying the list
+print(a)
