d5e3b4044919d89ab470180be3489b3e7cd9b683
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-16 20:16:39 +0530

    del statement

---
+++ 6. Data Structures/Lists/Methods/8_del_statement.py
@@ -0,0 +1,12 @@
+# Defining the list 
+a = [10, 20, 30, 40, 50]
+
+# Deleting an element from the list by using 'del' statement
+del a[0]
+
+# Displaying the list
+print(a)
+
+# Iterating the list 
+for item in a :
+    print(item, end=" ")
