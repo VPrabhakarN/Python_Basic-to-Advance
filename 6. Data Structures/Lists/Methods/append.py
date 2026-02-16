0604f7c8fa4e1a8d2a35ecf2cb287f8edcbe2fad
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-16 19:37:53 +0530

    append()

--- 6. Data Structures/Lists/Methods/append.py
+++ 6. Data Structures/Lists/Methods/append.py
@@ -1,1 +1,18 @@
-#
+# Defining list
+a = [10, 20, 30, 40, 50]
+
+# Appending elements to the list by using 'append()' method
+a.append(60)
+
+# Displaying the list 
+print(a)
+
+# Iterating the list (1st way)
+for item in a :
+    print(item, end=", ")
+    
+print()
+    
+# Iterating the list (2nd way)
+for i in range(len(a)) :
+    print(a[i], end=", ")
