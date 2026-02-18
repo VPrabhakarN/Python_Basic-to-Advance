439340a827573e7491b0bf7ac2954665cfc00c99
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-18 23:48:24 +0530

    Solution - 1 | Question 01

---
+++ 6. Data Structures/Lists/Exercise 01 (Scribd)/Solution_1.py
@@ -0,0 +1,11 @@
+# Question : Write a Python program to sum all the items in a list.
+
+# Defining a list
+numbers = [10, 101, 230, 7, 12, 72, 19]
+
+# Calculating the sum of all the items in a list
+sum = 0
+for number in numbers :
+    sum += number
+    
+# Displaying the sum of all the items in a list
