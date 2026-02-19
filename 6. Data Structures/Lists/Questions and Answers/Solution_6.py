fc7ef19a48f1d6117766d56485112fb46e53b54f
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-19 19:44:10 +0530

    Solved Question 06 | List Module

---
+++ 6. Data Structures/Lists/Questions and Answers/Solution_6.py
@@ -0,0 +1,13 @@
+# Question : Right Rotation by 1 : Shift all the elements one position to the right, with the first element moving to end.
+
+# Defining the list
+numbers = [10, 20, 30, 40, 50]
+
+print(f"List before : {numbers}")
+
+# Shifting all the elements one position to the left
+for i in range(len(numbers)-1, 0, -1) :
+    numbers[i], numbers[i-1] = numbers[i-1], numbers[i]
+    
+# Displaying the result
+print(f"List after : {numbers}")
