191411eea0b41ef3619bd61c1e051671a31823e5
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-19 21:21:31 +0530

    Got Question 07 | List Module

--- 6. Data Structures/Lists/Questions and Answers/Solution_7.py
+++ 6. Data Structures/Lists/Questions and Answers/Solution_7.py
@@ -1,1 +1,15 @@
 # Question : Reverse the list (In-Place) : Reverse the entire list without using extra space (i.e., swap elements).
+
+# Defining a list 
+numbers = [10, 13, 12, 32, 45, 19]
+
+print(f"List Before : {numbers}")
+
+# Reversing the list
+num = len(numbers)-1
+
+for i in range (len(numbers)//2) :
+    numbers[i], numbers[num] = numbers[num], numbers[i]
+    num -= 1
+    
+print(f"List After : {numbers}")
