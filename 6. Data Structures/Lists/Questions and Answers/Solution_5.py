cb9e1000fd0a6e5b78707636153b35d67f045347
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-19 19:33:22 +0530

    Added Comments

---
+++ 6. Data Structures/Lists/Questions and Answers/Solution_5.py
@@ -0,0 +1,6 @@
+# Question : Left Rotation by 1 -> Shift all the elements one position to the left, with the first element moving to end.
+
+# Defining the list
+numbers = [10, 20, 30, 40, 50]
+
+# Shifting all the elements one position to the left
