40bfd17293e1181c26ecfb63dbadca58bdf56230
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-19 22:19:23 +0530

    Added Comments

--- 6. Data Structures/Lists/Questions and Answers/Solution_6.py
+++ 6. Data Structures/Lists/Questions and Answers/Solution_6.py
@@ -10,4 +10,4 @@ for i in range(len(numbers)-1, 0, -1) :
     numbers[i], numbers[i-1] = numbers[i-1], numbers[i]
     
 # Displaying the result
-print(f"List after : {numbers}")
+print(f"List after : {numbers}") 
