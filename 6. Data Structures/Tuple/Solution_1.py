fb931515ddc72ee5a4964d42b2e66ded31377cc8
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-24 20:33:42 +0530

    Solved Question 01 | Tuple Module

---
+++ 6. Data Structures/Tuple/Solution_1.py
@@ -0,0 +1,12 @@
+# Question : Sum of tuple elements
+
+# Defining a tuple 
+data = (12, 13, 55, 76)
+
+# Calculating the data
+sum = 0
+
+for item in data :
+    sum += item
+    
+print(f"Sum of tuple elements : {sum}")
