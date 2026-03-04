dd46534f98bcf5f5944b18fc12592d5479a1d0f2
VPrabhakarN <vijaynagane19@gmail.com>
2026-03-04 13:19:55 +0530

    Got Solution | Question 5 | Set Module

---
+++ 6. Data Structures/C. Set/Solution_5.py
@@ -0,0 +1,9 @@
+# Question : Find the difference between two sets.
+
+def difference(s1, s2) :
+    return (s1-s2)
+
+s1 = {1, 3, 5, 6, 7, 8}
+s2 = {1, 2, 4, 5, 7, 8, 9}
+
+print(f"Difference ( Set 1 - Set 2) : {difference(s1, s2)}")
