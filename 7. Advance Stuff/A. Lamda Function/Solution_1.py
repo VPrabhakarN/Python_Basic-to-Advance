32afa84ba9ccf010f2267e5235aee6289a32fc78
VPrabhakarN <vijaynagane19@gmail.com>
2026-03-04 14:20:32 +0530

    Got Solution  | Question - 1 | Advance Stuff Module

---
+++ 7. Advance Stuff/A. Lamda Function/Solution_1.py
@@ -0,0 +1,7 @@
+# Question : Sort a list of integers using lambda.
+
+nums = [11, 2, 5, 3, 8, 3, 9, 2, 1, 10, 5]
+
+sorted_list = sorted(nums, key=lambda x:x)
+
+print(f"Sorted List : {sorted_list}")
