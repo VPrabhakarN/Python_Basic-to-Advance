31070a013c321d5591afb43558045aa468bcad2e
VPrabhakarN <vijaynagane19@gmail.com>
2026-03-05 15:38:37 +0530

    Solved Question | File Handling Module

---
+++ 9. File Handling/Beginner Level/Solution_1.py
@@ -0,0 +1,8 @@
+# Question : Write a program to read a file story.txt and print its entire content.
+
+# Reading file 
+with open("9. File Handling\Beginner Level\story.txt", "r") as file :
+    data = file.read()
+    print(data)
+    
+print("\n Closed the file \n")
