cd7f4b7e36814a2b11ccd95e5570256f904b5c2c
VPrabhakarN <vijaynagane19@gmail.com>
2026-03-05 15:54:47 +0530

    Solved Question | File Handling Module

---
+++ 9. File Handling/Beginner Level/Solution_2.py
@@ -0,0 +1,20 @@
+# Question : Read a file and print how many lines it contains?
+
+# 1st Way 
+with open("9. File Handling\Beginner Level\story.txt", "r") as file :
+    lines = file.readlines()
+    
+    print(f"Lines : {len(lines)}")
+
+# 2nd Way
+with open("9. File Handling\Beginner Level\story.txt", "r") as file :
+    count = 0
+    for line in file :
+        count += 1
+        
+    print(f"Lines : {count}")
+    
+
+# 3rd Way 
+with open("9. File Handling\Beginner Level\story.txt", "r") as file :
+    print(f"Lines : {sum(1 for line in file)}")
